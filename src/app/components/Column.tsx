89dea135c80d248dee5152a21f8688349a807b97
Nikhil <nikhilganeshpatil@gmail.com>
2024-01-19 06:17:18 +0530

    I have ensured the correct task type is selected when we push a new task to the db.

--- src/app/components/Column.tsx
+++ src/app/components/Column.tsx
@@ -19,8 +19,16 @@ const idToColumnText: { [key in TypedColumn]: string } = {
 }
 
 function ColumnElement({ id, todos, index }: Props) {
-  const [searchString] = useBoardStore((state) => [state.searchString])
+  const [searchString, setNewTaskType] = useBoardStore((state) => [
+    state.searchString,
+    state.setNewTaskType,
+  ])
   const [openModal] = useModalStore((state) => [state.openModal])
+
+  const handleAddTodo = () => {
+    setNewTaskType(id)
+    openModal()
+  }
 
   return (
     <Draggable draggableId={id} index={index}>
@@ -84,7 +92,7 @@ function ColumnElement({ id, todos, index }: Props) {
                   <div className='flex items-end justify-end p-2'>
                     <button className='text-green-500 hover:text-green-600'>
                       <PlusCircleIcon
-                        onClick={openModal}
+                        onClick={handleAddTodo}
                         className='h-10 w-10'
                       ></PlusCircleIcon>
                     </button>
